a36d02d5be751d7fcbe63cc03d8065badbab27ea
Abdallah-Mohamed-Abdallah-AbdulRazzaq <abdallahmohammad66446@gmail.com>
2023-04-13 01:05:36 +0200

    Add Widget home and import (flower_app/login)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-
-
+import 'package:flower_app/pages/login.dart';
+import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: testttttt(),
+      home: Login(),
     );
   }
 }
