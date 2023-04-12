b78a24dbeece40524f5a0b65a7c1fa9c2b18b26d
Abdallah-Mohamed-Abdallah-AbdulRazzaq <abdallahmohammad66446@gmail.com>
2023-04-13 01:39:17 +0200

    add Padding in Column

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -7,22 +7,25 @@ class Login extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          children: [
-            const SizedBox(height: 64,),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              obscureText: false,
-              decoration: InputDecoration(
-                hintText: "Enter Your Email : ",
-                enabledBorder: OutlineInputBorder(borderSide: Divider.createBorderSide(context),),
-                focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey,),),
-                filled: true,
-                contentPadding: const EdgeInsets.all(8),
-                )
-            )
-          ],
-      
+        child: Padding(
+          padding: const EdgeInsets.all(33.0),
+          child: Column(
+            children: [
+              const SizedBox(height: 64,),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                obscureText: false,
+                decoration: InputDecoration(
+                  hintText: "Enter Your Email : ",
+                  enabledBorder: OutlineInputBorder(borderSide: Divider.createBorderSide(context),),
+                  focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey,),),
+                  filled: true,
+                  contentPadding: const EdgeInsets.all(8),
+                  )
+              )
+            ],
+              
+          ),
         ),
       ),
     );
