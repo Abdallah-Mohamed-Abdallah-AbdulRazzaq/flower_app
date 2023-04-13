2893ea847a9ae7c51f6145a74bf3d49bc2c47f0a
Abdallah-Mohamed-Abdallah-AbdulRazzaq <abdallahmohammad66446@gmail.com>
2023-04-13 19:16:18 +0200

    add widget Text and mainAxisAlignment

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,28 +7,39 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: Drawer(
-          child: Column(
-        children: [
-          UserAccountsDrawerHeader(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage("assets/img/pk.jpg"), fit: BoxFit.cover),
-            ),
-            currentAccountPicture: CircleAvatar(
-              radius: 55,
-              backgroundImage: AssetImage("assets/img/my.jpg"),
-            ),
-            accountEmail: Text("Abdallah@yahoo.com"),
-            accountName: Text("Abdallah Mohamed",
-                style: TextStyle(
-                  color: Color.fromARGB(255, 255, 255, 255),
-                )),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+          Column(
+            children: [
+                UserAccountsDrawerHeader(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/img/pk.jpg"), fit: BoxFit.cover),
+                ),
+                currentAccountPicture: CircleAvatar(
+                  radius: 55,
+                  backgroundImage: AssetImage("assets/img/my.jpg"),
+                ),
+                accountEmail: Text("Abdallah@yahoo.com"),
+                accountName: Text("Abdallah Mohamed",
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                    )),
+              ),
+               
+               
+                ListTile(title: Text("Home"), leading: Icon(Icons.home), onTap: () {}),
+                ListTile(title: Text("My products"),leading: Icon(Icons.add_shopping_cart),onTap: () {}),
+                ListTile(title: Text("About"),leading: Icon(Icons.help_center),onTap: () {}),
+                ListTile(title: Text("Logout"),leading: Icon(Icons.exit_to_app),onTap: () {}),
+                
+            ],
           ),
-          ListTile(title: Text("Home"), leading: Icon(Icons.home), onTap: () {}),
-          ListTile(title: Text("My products"),leading: Icon(Icons.add_shopping_cart),onTap: () {}),
-          ListTile(title: Text("About"),leading: Icon(Icons.help_center),onTap: () {}),
-          ListTile(title: Text("Logout"),leading: Icon(Icons.exit_to_app),onTap: () {}),
-        ],
+
+        Text("data")    
+                 
+                  ],
       )),
       appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 76, 141, 95),
