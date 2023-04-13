8963a8eb783be5567d7991300b4c65c75b258689
Abdallah-Mohamed-Abdallah-AbdulRazzaq <abdallahmohammad66446@gmail.com>
2023-04-13 15:19:22 +0200

    Add SafeArea in ( Scaffold )

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -10,45 +10,47 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromARGB(255, 247, 247, 247),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(33.0),
-          child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(height: 64,),
-              
-              MyTextField(hinttexttt: 'Enter Your Email', isPassword: false, textInputTypeee: TextInputType.emailAddress,),
-              const SizedBox(height: 33,),
-              MyTextField(hinttexttt: 'Enter Your Password', isPassword: true, textInputTypeee: TextInputType.text,),
-              
-              const SizedBox(height: 33,),
-
-              ElevatedButton(onPressed: (){}, child: Text( ' Sing in ' , style: TextStyle(fontSize: 19) ) ,
-              style: ButtonStyle(backgroundColor: MaterialStateProperty.all(BTNgreen),
-              padding: MaterialStateProperty.all(EdgeInsets.all(12)),
-              shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8)
-              )),
-              ),
-              ),
-
-              const SizedBox(height: 33,),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Do not have an account?' , style: TextStyle(fontSize: 18),),
-                  TextButton(onPressed: (){}, child: Text('sing up' , style: TextStyle(color: Colors.black , fontSize: 18))),
-                ],
-              )
-
-
-
-            ],
-      
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color.fromARGB(255, 247, 247, 247),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(33.0),
+            child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 64,),
+                
+                MyTextField(hinttexttt: 'Enter Your Email', isPassword: false, textInputTypeee: TextInputType.emailAddress,),
+                const SizedBox(height: 33,),
+                MyTextField(hinttexttt: 'Enter Your Password', isPassword: true, textInputTypeee: TextInputType.text,),
+                
+                const SizedBox(height: 33,),
+    
+                ElevatedButton(onPressed: (){}, child: Text( ' Sing in ' , style: TextStyle(fontSize: 19) ) ,
+                style: ButtonStyle(backgroundColor: MaterialStateProperty.all(BTNgreen),
+                padding: MaterialStateProperty.all(EdgeInsets.all(12)),
+                shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8)
+                )),
+                ),
+                ),
+    
+                const SizedBox(height: 33,),
+    
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Do not have an account?' , style: TextStyle(fontSize: 18),),
+                    TextButton(onPressed: (){}, child: Text('sing up' , style: TextStyle(color: Colors.black , fontSize: 18))),
+                  ],
+                )
+    
+    
+    
+              ],
+        
+            ),
           ),
         ),
       ),
