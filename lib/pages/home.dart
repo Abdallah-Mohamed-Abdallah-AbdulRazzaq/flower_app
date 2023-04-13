2369cae99e9d570a6e9fe453fd41f980bb236f68
Abdallah-Mohamed-Abdallah-AbdulRazzaq <abdallahmohammad66446@gmail.com>
2023-04-13 16:38:06 +0200

    add IconButton in AppBar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -14,6 +14,9 @@ class Home extends StatelessWidget {
             children: [Container(
               child: Text("8",style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),),
                 padding: EdgeInsets.all(5) , decoration: BoxDecoration (color: Color.fromARGB(211, 164, 255, 193) , shape: BoxShape.circle)),
+              
+              IconButton(onPressed: () {}, icon: Icon(Icons.add_shopping_cart)),
+              
                 ],
               ),
 
