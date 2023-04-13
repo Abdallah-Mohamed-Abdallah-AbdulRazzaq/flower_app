c0a62b107d8cf29480f28665d8fb1b4c6201380c
Abdallah-Mohamed-Abdallah-AbdulRazzaq <abdallahmohammad66446@gmail.com>
2023-04-13 20:24:54 +0200

    add widget ( top , bottom , right , left )

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -16,9 +16,10 @@ class Home extends StatelessWidget {
           return GridTile( 
             child: Stack(children:[
              Positioned(
-              
-
-
+              top: -3,
+              bottom: -9,
+              right: 0,
+              left: 0,
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(55),
                 child: Image.asset("assets/img/1.jpg"),
