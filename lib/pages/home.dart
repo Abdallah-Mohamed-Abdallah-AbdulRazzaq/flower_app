bcf3d3c494b5d857bb423cf3691967c5bec0823a
Abdallah-Mohamed-Abdallah-AbdulRazzaq <abdallahmohammad66446@gmail.com>
2023-04-13 18:34:51 +0200

    Add ListTile and widget (title , leading , onTap )

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,35 +10,52 @@ class Home extends StatelessWidget {
           child: Column(
         children: [
           UserAccountsDrawerHeader(
-            decoration: BoxDecoration(image: DecorationImage(image: AssetImage("assets/img/pk.jpg"),fit: BoxFit.cover),),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/img/pk.jpg"), fit: BoxFit.cover),
+            ),
             currentAccountPicture: CircleAvatar(
               radius: 55,
               backgroundImage: AssetImage("assets/img/my.jpg"),
             ),
             accountEmail: Text("Abdallah@yahoo.com"),
-            accountName: Text("Abdallah Mohamed",style: TextStyle(
-                  color: Color.fromARGB(255, 255, 255, 255),)),
+            accountName: Text("Abdallah Mohamed",
+                style: TextStyle(
+                  color: Color.fromARGB(255, 255, 255, 255),
+                )),
           ),
+          
+          ListTile(
+              title: Text("Home"), leading: Icon(Icons.home), onTap: () {}),
+
+              
         ],
       )),
       appBar: AppBar(
-      backgroundColor: Color.fromARGB(255, 76, 141, 95),
-      title: Text("Home"),
-      actions: [
-        Row(
-      children: [
-        Stack(
-      children: [
-        Container(
-          child: Text( "8", style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),),
-
-          padding: EdgeInsets.all(5),decoration: BoxDecoration(color: Color.fromARGB(211, 164, 255, 193),shape: BoxShape.circle)),
-          
-          IconButton(onPressed: () {}, icon: Icon(Icons.add_shopping_cart)),
+        backgroundColor: Color.fromARGB(255, 76, 141, 95),
+        title: Text("Home"),
+        actions: [
+          Row(
+            children: [
+              Stack(
+                children: [
+                  Container(
+                      child: Text(
+                        "8",
+                        style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+                      ),
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                          color: Color.fromARGB(211, 164, 255, 193),
+                          shape: BoxShape.circle)),
+                  IconButton(
+                      onPressed: () {}, icon: Icon(Icons.add_shopping_cart)),
                 ],
               ),
-          
-          Padding(padding: const EdgeInsets.only(right: 12),child: Text("\$ 128"),)
+              Padding(
+                padding: const EdgeInsets.only(right: 12),
+                child: Text("\$ 128"),
+              )
             ],
           )
         ],
